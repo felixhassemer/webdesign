aa6eccb6f58c25e1cda8a24abad6af1b3bcd2931
felixhassemer <felixhassemer@gmail.com>
2018-03-03 14:58:54 +0100

    component renaming and removal

--- portfolio-site/src/App.vue
+++ portfolio-site/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <html lang="de">
+
+  <head>
+    <!-- <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Felix Hassemer | Designer</title> -->
+    <p5Circles></p5Circles>
+  </head>
+
+  </html>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import p5Circles from './components/p5Circles'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    p5Circles
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ portfolio-site/src/components/p5Circles.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'p5Circles',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
