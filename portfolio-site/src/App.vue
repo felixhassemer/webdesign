97914387e6612bcdd488f52135a05da192b0984c
felixhassemer <felixhassemer@gmail.com>
2018-03-06 16:54:39 +0100

    testing basic stuff

--- portfolio-site/src/App.vue
+++ portfolio-site/src/App.vue
@@ -2,13 +2,11 @@
   <html lang="de">
 
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- <title>Felix Hassemer | Designer</title> -->
   </head>
   <body>
     <p5Circles></p5Circles>
+    <canv></canv>
   </body>
 
   </html>
@@ -19,18 +17,15 @@
 <script src="./js/sketch.js"></script>
 
 <script>
-import p5Circles from './components/p5Circles';
-// import p5js from '../libraries/p5.js';
-import p5 from "../node_modules/p5/lib/p5.js";
-// Object.definePrototype(vm.prototype, '$p5', { value: p5 });
+import p5Circles from './components/p5Circles.vue';
+import canv from './components/canvas.vue';
+
 
 export default {
   name: 'App',
   components: {
-    p5Circles
-  },
-  mounted: function(){
-    require('../node_modules/p5/lib/p5.js')
+    p5Circles,
+    canv
   }
 }
 </script>
--- portfolio-site/src/components/p5Circles.vue
+++ portfolio-site/src/components/p5Circles.vue
@@ -1,39 +1,35 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-
-    <div id="canvas">
-    </div>
-    <button v-on:click="clicked">Click here</button>
+    <div
+      class="colorBox"
+      :style="{backgroundColor: color}"
+    ></div>
+    <input type="text" v-model="color">
   </div>
 
 </template>
 
 <script>
-// import p5 from '../../libraries/p5.js';
-import p5 from "../../node_modules/p5/lib/p5.js"
-
-
 export default {
   name: 'p5Circles',
-  data () {
+  data() {
     return {
-      msg: 'Hello World'
+      color: 'red'
     }
-  },
-  mounted: function(){
-    require('../../node_modules/p5/lib/p5.js');
   },
   methods: {
-    clicked: function() {
-      // p5.createCanvas(200,200);
-      p5.background(0);
-    }
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.colorBox {
+  width: 200px;
+  height: 200px;
+  /* background-color: red; */
+}
 
 </style>
