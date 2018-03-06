3d8a685945393a590c29ecfd7d771ef9d62c7c90
felixhassemer <felixhassemer@gmail.com>
2018-03-06 19:20:03 +0100

    initial commit gallery component

---
+++ portfolio-site/src/components/gallery.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="gallery">
+    <ul>
+      <li><p>Lorem Ipsum</p></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'gallery',
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
