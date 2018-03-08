880ea09509014d44524370828e067ccc09bd049b
felixhassemer <felixhassemer@gmail.com>
2018-03-08 17:41:47 +0100

    initial commit top section component

---
+++ portfolio-site/src/components/topsection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="topsection">
+    <div id="logo"></div>
+    <div class="">
+      <h1>Hi, I'm Felix.</h1>
+      <p>I'm a freelance designer / graphic artist from Trier, Germany. I work with typography, generative design and 3D.</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'topsection',
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
