5450f9d91765e05d6a91ccab28aa0ecc7b16fdb5
felixhassemer <felixhassemer@gmail.com>
2018-03-09 12:14:57 +0100

    navigation menu initial commit

---
+++ portfolio-site/src/components/navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="navigation">
+    <img id="logo" src="../assets/logo.png" alt="Felix Hassemer Logo"/>
+    <div id="links">
+      <a href="">about</a>
+      <a href="">contact</a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'navigation'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#navigation {
+  /* background-color: #555; */
+  position: fixed;
+
+  width: 100vw;
+}
+
+#logo {
+  float: left;
+  padding: 20px;
+  width: 15vw;
+  max-width: 80px;
+}
+
+#links {
+  float: right;
+
+}
+
+</style>
