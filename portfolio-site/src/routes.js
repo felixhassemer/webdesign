8bfe3ba1e8bc8b770f936a238e6a6ccdb989927a
felixhassemer <felixhassemer@gmail.com>
2018-03-10 23:37:16 +0100

    routes for projects added

--- portfolio-site/src/routes.js
+++ portfolio-site/src/routes.js
@@ -2,8 +2,25 @@ import About from './components/About.vue';
 import Contact from './components/Contact.vue';
 import Gallery from './components/Gallery.vue';
 
+// project pages
+import Bachelor from './components/projects/Bachelor.vue';
+import BachelorExhibition from './components/projects/BachelorExhibition.vue';
+import Voyager from './components/projects/Voyager.vue';
+import AbstractPosters from './components/projects/AbstractPosters.vue';
+import MonsExhibition from './components/projects/MonsExhibition.vue';
+
+
+
 export const routes = [
-  { path: '', component: Gallery },
+  // main page routes
+  { path: '/', component: Gallery },
   { path: '/about', component: About },
-  { path: '/contact', component: Contact }
+  { path: '/contact', component: Contact },
+
+  // project routes
+  { path: '/projects/bachelor', component: Bachelor },
+  { path: '/projects/bachelor_exhibition', component: BachelorExhibition },
+  { path: '/projects/voyager', component: Voyager },
+  { path: '/projects/abstract_posters', component: AbstractPosters },
+  { path: '/projects/mons_exhibition', component: MonsExhibition },
 ];
